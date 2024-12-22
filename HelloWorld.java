aced4d3b7d99c56539fdabfb09e32ad96e058857
Mayuri Peddawad <155711062+MayuriPeddawad@users.noreply.github.com>
2024-12-22 15:05:34 +0530

    Add files via upload
    First java program with basic syntax

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package Mayuri_Peddawad;
+
+public class HelloWorld {
+    public void mayu() {
+        System.out.println("Hello");
+    }
+        public static void main(String[] args) 
+        {
+        System.out.println("Hello World");
+        HelloWorld hw = new HelloWorld(); 
+        hw.mayu(); 
+        
+    }
+    }
+
