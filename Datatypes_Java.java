107ccfa7a6a510951f7185bd5a647a2dac4ca263
Mayuri Peddawad <155711062+MayuriPeddawad@users.noreply.github.com>
2024-12-22 15:42:45 +0530

    Add files via upload

---
+++ Datatypes_Java.java
@@ -0,0 +1,21 @@
+package Mayuri_Peddawad;
+
+public class Datatypes_Java {
+
+	public static void main(String[] args) {
+		
+		int myintnum = 4;
+		float myfloatnum = 4.3f;
+		char myletter = 'M'; 
+		boolean mybool = true;
+		String mytext = "Helloworld";
+		
+		System.out.println("Integer is " + myintnum); // whole number (1,2,3.....,n)
+		System.out.println("Float is " + myfloatnum); // Floating point number 
+		System.out.println("Char is " + myletter);  // only one letter is display
+		System.out.println("Boolean is " + mybool);  // return values in True and False 
+        System.out.println("String is " + mytext);	// text format is display	
+	}
+
+}
+
