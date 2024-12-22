a72bc6a65d6f8ae1bf35af56a3c1bed10d71fe47
Mayuri Peddawad <155711062+MayuriPeddawad@users.noreply.github.com>
2024-12-22 15:20:30 +0530

    Update HelloWorld.java
    Notes Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,21 @@
 package Mayuri_Peddawad;
 
 public class HelloWorld {
-    public void mayu() {
-        System.out.println("Hello");
+    public void mayu() {         // method declaration 
+        System.out.println("Hello"); // pass print statement by mayu function
     }
         public static void main(String[] args) 
         {
-        System.out.println("Hello World");
-        HelloWorld hw = new HelloWorld(); 
-        hw.mayu(); 
+        System.out.println("Hello World"); // pass print statement from main method
+        HelloWorld hw = new HelloWorld(); // created object of HelloWorld class
+        hw.mayu();   // call method by created object 
         
     }
     }
 
+Notes :
+* Java is object-oriented programming language.
+* main() is very important in class without main() method we can't run program.
+* Class : it is like class constructor or "blueprint" for creating objects .
+* Object : it is cretaed from a class .
+- we can create multiple object of single class and multiple class .
